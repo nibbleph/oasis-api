4ccd4e0ce45f8754ed5d690634b43839aa6c2ee1
wendalefranz.dy <wendalefranz.dy@gmail.com>
2023-12-11 23:38:26 +0800

    UPDATE -- code changes

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -30,7 +30,7 @@ class EmailController extends Controller
         $pdf = PDF::loadView($view, $request->all());
         $pdf->save(public_path('generated.pdf'));
 
-        Mail::to($request->email)->bcc('support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com')->send(new SendForm());
+        Mail::to($request->email)->cc(['support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com'])->send(new SendForm());
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
         // return view('clientele', $request->all());
     }
@@ -44,21 +44,21 @@ class EmailController extends Controller
             'subject' => 'required|string',
             'message' => 'required|string'
         ]);
-        Mail::to($request->email)->bcc('support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com')->send(new SendContact($request->email, $request->name, $request->subject, $request->message));
+        Mail::to($request->email)->cc(['support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com'])->send(new SendContact($request->email, $request->name, $request->subject, $request->message));
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
     }
 
     public function send_form(Request $request){
         $pdf = PDF::loadView('insurance', $request->all());
         $pdf->save(public_path('generated.pdf'));
-        Mail::to($request->email)->bcc('support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com')->send(new SendForm());
+        Mail::to($request->email)->cc(['support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com'])->send(new SendForm());
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
     }
 
     public function send_pricing(Request $request){
         $pdf = PDF::loadView('pricing_form', $request->all());
         $pdf->save(public_path('generated.pdf'));
-        Mail::to($request->email)->bcc('support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com')->send(new SendForm());
+        Mail::to($request->email)->cc(['support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com'])->send(new SendForm());
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
     }
 
@@ -67,7 +67,7 @@ class EmailController extends Controller
     public function send_support(Request $request){
         $pdf = PDF::loadView('customized_support', $request->all());
         $pdf->save(public_path('generated.pdf'));
-        Mail::to($request->email)->bcc('support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com')->send(new SendForm());
+        Mail::to($request->email)->cc(['support@oasisdentalcallcenter.com', 'joegabucan@protonmail.com', 'rejashannehrose@gmail.com'])->send(new SendForm());
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
     }
 
@@ -75,14 +75,14 @@ class EmailController extends Controller
     public function web_development(Request $request){
         $pdf = PDF::loadView('webdev_form', $request->all());
         $pdf->save(public_path('generated.pdf'));
-        Mail::to($request->email)->bcc('nibbleph.dev@gmail.com', 'wendalefranz.dy@gmail.com', 'cedricmatthewv@gmail.com', 'arjohn.ely45@gmail.com')->send(new SendForm());
+        Mail::to($request->email)->cc(['nibbleph.dev@gmail.com', 'wendalefranz.dy@gmail.com', 'cedricmatthewv@gmail.com', 'arjohn.ely45@gmail.com'])->send(new SendForm());
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
     }
 
     public function digital_marketing(Request $request){
         $pdf = PDF::loadView('digitalmarketing_form', $request->all());
         $pdf->save(public_path('generated.pdf'));
-        Mail::to($request->email)->bcc('nibbleph.dev@gmail.com', 'wendalefranz.dy@gmail.com', 'cedricmatthewv@gmail.com', 'arjohn.ely45@gmail.com', 'rejashannehrose@gmail.com')->send(new SendForm());
+        Mail::to($request->email)->cc(['nibbleph.dev@gmail.com', 'wendalefranz.dy@gmail.com', 'cedricmatthewv@gmail.com', 'arjohn.ely45@gmail.com', 'rejashannehrose@gmail.com'])->send(new SendForm());
         return response()->json(['message' => 'Successfully Sent!', 'status' => 1]);
     }
 
